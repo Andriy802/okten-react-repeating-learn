f7eb70e413d7b50c2916e4e3219b17e4699b84e5
Andriy802 <Agavanchak@gmail.com>
2025-10-08 20:29:24 +0100

    lesson 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import {Characters} from "./components/Characters/Characters";
 
 const App = () => {
     return (
         <div>
-            App
+            <Characters />
         </div>
     );
 };
